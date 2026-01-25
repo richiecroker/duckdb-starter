f05a4cc29fba3030f6bb8d703fe9215fe8ab20c1
Richard Croker <ra.croker@gmail.com>
2026-01-25 22:28:40 +0000

    test query

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,19 @@
+# pages/duckdb.py
+import re
 import streamlit as st
+import duckdb
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas as pd
+import itertools
+
 st.title("Main app (app.py)")
+
+
+duckdb_query = """
+SELECT * from practices
+"""
+
+result = conn.execute(duckdb_query).fetchdf()
+st.dataframe(result)
+st.caption(f"Returned {len(result):,} rows")
